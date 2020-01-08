6f745dc2a11facd4b8b0346db853f3692fbff87e
Luis Alejo <lou.m.alejo@gmail.com>
2020-01-08 21:23:11 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -11,7 +11,6 @@ end
 def division(num1, num2)
   result = num1 / num2
   result
-
 end
 
 def multiplication(num1, num2)
@@ -20,7 +19,8 @@ def multiplication(num1, num2)
 end
 
 def modulo(num1, num2)
-
+  result = num2 % num1
+  result
 end
 
 def square_root(num)
